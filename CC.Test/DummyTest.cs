625110beb79f701c7d3b6338a79a5a44a9445fd5
samranjbari <samranjbari@gmail.com>
2014-01-25 16:09:31 -0500

    added test solution

---
+++ CC.Test/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CC.Test
+{
+    [TestClass]
+    public class DummyTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
