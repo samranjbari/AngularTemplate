47f5b3a001de0e1f08a39cedcec2966afee79031
samranjbari <samranjbari@gmail.com>
2014-01-25 16:32:29 -0500

    fixed the test

--- CC.Test/DummyTest.cs
+++ CC.Test/DummyTest.cs
@@ -9,7 +9,7 @@ namespace CC.Test
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(1, 2);
+            Assert.AreEqual(2, 2);
         }
 
         [TestMethod]
